a5727a3aace31c9a07be26b6360ba765ce4d6153
tonyamit999 <52860086+tonyamit999@users.noreply.github.com>
2019-07-13 17:48:28 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+﻿AFRAME.registerComponent('vidhandler', {
+    init: function () {
+        this.toggle = false;
+        document.querySelector("#video").pause(); //reference to the video
+    },
+    tick: function () {
+        if (document.querySelector("a-marker").object3D.visible == true) {
+            if (!this.toggle) {
+                this.toggle = true;
+                document.querySelector("#video").play();
+            }
+        } else {
+            this.toggle = false;
+            document.querySelector("#video").pause();
+        }
+    }
+});
